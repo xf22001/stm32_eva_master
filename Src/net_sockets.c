ade869c7b8bfb15329e73b0a6e5db8a0dcacaa97
xiaofei <alibaba22001@163.com>
2022-08-29 14:00:55 +0800

    update

--- Src/net_sockets.c
+++ Src/net_sockets.c
@@ -4,22 +4,21 @@
   * File Name       : net_sockets.c.h
   * Description     : TCP/IP or UDP/IP networking functions implementation based
                     on LwIP API see the file "mbedTLS/library/net_socket_template.c"
-                    for the standard implmentation
+                    for the standard implementation
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
   *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
 /*
- * This is a template implmentation of the net_socket.c based on the LwIP
+ * This is a template implementation of the net_socket.c based on the LwIP
  * TCP/IP Stack.
  *
  */
