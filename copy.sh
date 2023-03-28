f9402c837f64b4ba6ada850ef7cc2dd819980365
xiaofei <alibaba22001@163.com>
2023-03-28 15:35:05 +0800

    update

--- copy.sh
+++ copy.sh
@@ -6,14 +6,20 @@
 #   文件名称：copy.sh
 #   创 建 者：肖飞
 #   创建日期：2021年08月02日 星期一 15时31分26秒
-#   修改日期：2021年09月15日 星期三 10时41分48秒
+#   修改日期：2023年03月28日 星期二 15时33分44秒
 #   描    述：
 #
 #================================================================
 function main() {
 	cat cscope/cscope.files | while read line; do
-		echo rsync -aR $line ./src | grep "/eva_charger/\|/stm32_apps/\|/sal/\|/cJSON/" | sed 's:"::g' | sed 's:/eva_charger/:/eva_charger/./:g' | sed 's:/stm32_apps/:/stm32_apps/./:g' | sed 's:/sal/:/./sal/:g' | sed 's:/cJSON/:/./cJSON/:g'
+		echo rsync -aR $line ./src | grep "/eva_master/\|/stm32_apps/\|/sal/\|/cJSON/" | sed 's:"::g' | sed 's:/eva_master/:/eva_master/./:g' | sed 's:/stm32_apps/:/stm32_apps/./:g' | sed 's:/sal/:/./sal/:g' | sed 's:/cJSON/:/./cJSON/:g'
 	done
+	echo rsync -aR "$(pwd)/./STM32F207VETx_FLASH.ld" ./src
+	echo rsync -aR "$(pwd)/./Makefile" ./src
+	echo rsync -aR "$(pwd)/./config.mk" ./src
+	echo rsync -aR "$(pwd)/./override.mk" ./src
+	echo rsync -aR "$(pwd)/./target.mk" ./src
+	echo rsync -aR "$(pwd)/./user.mk" ./src
 }
 
 main $@
